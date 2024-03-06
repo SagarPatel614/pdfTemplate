4f9b42e770ecb132bb5d8508adeda2127d658596
Sagar <scp1406@gmail.com>
2024-03-06 15:15:09 -0500

    Added lines and no-lines pdf

---
+++ main.py
@@ -0,0 +1,49 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+data = pd.read_csv("resource/topics.csv")
+
+
+def generate_pdf(lined=False):
+    for index, row in data.iterrows():
+        # Header of the page
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        if lined:
+            for y in range(20, 298, 10):
+                pdf.line(10, y, 200, y)
+        else:
+            pdf.line(10, 21, 200, 21)
+
+        # Fill the rest with lines
+        pdf.ln(265)
+
+        # Set the footer
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for p in range(row["Pages"] - 1):
+            pdf.add_page()
+            if lined:
+                for y in range(20, 298, 10):
+                    pdf.line(10, y, 200, y)
+            else:
+                pdf.line(10, 21, 200, 21)
+            # Fill the rest with lines
+            pdf.ln(277)
+            # Set the footer
+            pdf.set_font(family="Times", style="I", size=8)
+            pdf.set_text_color(180, 180, 180)
+            pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+    pdf.output("output.pdf")
+
+
+if "__name__" == "__main__":
+    lined = True
+    generate_pdf(lined)
